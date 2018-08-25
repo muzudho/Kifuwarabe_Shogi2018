7b6fac86c1f4f01788f1703e15a27a176940d8de
むずでょ <muzudho1@gmail.com>
2018-08-25 16:50:14 +0900

    マージン1秒☆（＾ｑ＾）

--- src/time_manager.rs
+++ src/time_manager.rs
@@ -1,10 +1,14 @@
+/// マージン 1000ミリ秒。
+const MARGIN_MILLISECONDS : u64 = 1000;
+
 /// 思考時間の上限。
 pub fn get_thought_max_milliseconds(milliseconds: u64) -> u64 {
-    let mut a = milliseconds / 2;
+    // マージン1秒は取っておく。
+    let mut a = milliseconds / 2 - MARGIN_MILLISECONDS;
 
     // マージン1秒として、 30秒以内に収める。
-    if 29_000 < a {
-        a = 29_000;
+    if 30_000 - MARGIN_MILLISECONDS < a {
+        a = 30_000 - MARGIN_MILLISECONDS;
     }
 
     a
